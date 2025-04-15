13a496d260e0df5368131f108c1c7fdf070a46a4
WolfyScript <wolfyscript@gmail.com>
2025-04-15 18:21:36 +0200

    test: update test servers to 1.21.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,14 +85,14 @@ minecraftServers {
     val debugPortMapping = "${debugPort}:${debugPort}"
     servers {
         register("spigot_1_21") {
-            version.set("1.21.1")
+            version.set("1.21.5")
             type.set("SPIGOT")
             extraEnv.put("BUILD_FROM_SOURCE", "true")
             imageVersion.set("java21-graalvm") // graalvm contains the jdk required to build from source
             ports.set(setOf(debugPortMapping, "25569:25565"))
         }
         register("paper_1_21") {
-            version.set("1.21")
+            version.set("1.21.5")
             type.set("PAPER")
             imageVersion.set("java21")
             ports.set(setOf("5007:5007", "25570:25565"))
